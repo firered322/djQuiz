11ada38f30077797679206d0bc6840040a28d5e9
Atul <atulsingh1396@gmail.com>
2020-06-18 08:11:28 +0530

    added home, styles and quiz & user models

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    question = models.CharField(max_length=200)
+    correct_ans = models.CharField(max_length=200)
+    wrong_ans1 = models.CharField(max_length=200)
+    wrong_ans2 = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question
