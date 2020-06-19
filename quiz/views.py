0de953d0bd873fbe6942b13e7a6302fe3abe69cb
Atul <atulsingh1396@gmail.com>
2020-06-19 08:19:28 +0530

    added answer choices as list items

--- quiz/views.py
+++ quiz/views.py
@@ -4,5 +4,7 @@ from .models import Question
 
 def home(request):
     question = Question.objects.get(id=1)
-    context = {'question': question}
+    answers = question.answer_set.all()
+    context = {'question': question, 'answers': answers}
+    # print(answers)
     return render(request, 'quiz/home.html', context)
