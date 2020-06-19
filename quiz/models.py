a17992547b80b3b518fc9ca37c158e2662215c53
Atul <atulsingh1396@gmail.com>
2020-06-19 07:50:24 +0530

    made model changes

--- quiz/models.py
+++ quiz/models.py
@@ -14,9 +14,16 @@ class User(models.Model):
 
 class Question(models.Model):
     question = models.CharField(max_length=200)
-    correct_ans = models.CharField(max_length=200)
-    wrong_ans1 = models.CharField(max_length=200)
-    wrong_ans2 = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.question
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=200)
+    is_correct = models.BooleanField(null=False)
+
+    def __str__(self):
+        return self.answer
